6d8314096a265c93632870604ee756c9db0fc686
evgenyantipin <evgeny.antipin69@gmail.com>
2018-09-14 19:35:01 +0300

    resolve conflict naming with russian gem

--- lib/translit.rb
+++ lib/translit.rb
@@ -8,7 +8,7 @@ module Translit
       detect_input_language(text.split(/\s+/).first)
     end
 
-    map = self.send(language.to_s).sort_by {|k,v| v.length <=>  k.length}
+    map = self.send("translit_#{language}").sort_by {|k,v| v.length <=>  k.length}
     map.each do |translit_key, translit_value|
       text.gsub!(translit_key.capitalize, translit_value.first)
       text.gsub!(translit_key, translit_value.last)
@@ -22,7 +22,7 @@ module Translit
 
 private
   def self.create_russian_map
-    self.english.inject({}) do |acc, tuple|
+    self.translit_english.inject({}) do |acc, tuple|
       rus_up, rus_low = tuple.last
       eng_value       = tuple.first
       acc[rus_up]  ? acc[rus_up]  << eng_value.capitalize : acc[rus_up]  = [eng_value.capitalize]
@@ -43,7 +43,7 @@ private
     end
   end
 
-  def self.english
+  def self.translit_english
     { "a"=>["А","а"], "b"=>["Б","б"], "v"=>["В","в"], "g"=>["Г","г"], "d"=>["Д","д"], "e"=>["Е","е"], "yo"=>["Ё","ё"], "jo"=>["Ё","ё"], "ö"=>["Ё","ё"], "zh"=>["Ж","ж"],
       "z"=>["З","з"], "i"=>["И","и"], "j"=>["Й","й"], "k"=>["К","к"], "l"=>["Л","л"], "m"=>["М","м"], "n"=>["Н","н"], "o"=>["О","о"], "p"=>["П","п"], "r"=>["Р","р"],
       "s"=>["С","с"], "t"=>["Т","т"], "u"=>["У","у"], "f"=>["Ф","ф"], "h"=>["Х","х"], "x"=>["Кс","кс"], "ts"=>["Ц","ц"], "ch"=>["Ч","ч"], "sh"=>["Ш","ш"], "w"=>["В","в"],
@@ -51,7 +51,7 @@ private
       "ü"=>["Ю","ю"], "ya"=>["Я","я"], "ja"=>["Я","я"], "q"=>["Я","я"]}
   end
 
-  def self.russian
+  def self.translit_russian
     @russian ||= create_russian_map
   end
 end
